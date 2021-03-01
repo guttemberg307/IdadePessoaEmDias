1c503775d57a98af73b16273de1ac9a6ac71207d
gutemberg3078 <gsilva@iblueconsulting.com.br>
2021-03-01 15:36:37 -0300

    Idade pessoa em dias

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdadePessoa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("============= IDADE DE UMA PESSOA EM DIAS =========================");
+
+            Console.Write("Insira o dia do seu nascimento: ");
+            int dia = int.Parse(Console.ReadLine());
+
+
+            Console.Write("Insira o mês do seu nascimento:");
+            int mes = int.Parse(Console.ReadLine());
+
+
+
+            Console.Write("Insira o ano do seu nascimento:");
+            int ano = int.Parse(Console.ReadLine());
+
+
+            int Mesdias = mes * 30;
+            int Anosdias = ano * 365;
+
+            int resultado = Mesdias + Anosdias + dia;
+
+            Console.WriteLine("O total de dias vividos de uma pessoa é de:{0}",resultado);
+
+          
+
+
+
+        }
+    }
+}
